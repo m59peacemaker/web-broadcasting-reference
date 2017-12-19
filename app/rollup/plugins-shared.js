ac0d42a8f5f5601134e203048b07d39ce44bc20e
Johnny Hauser <johnnyhauser@gmail.com>
2017-12-19 14:01:14 -0600

    will find a way to fix and use buble, tired of the chaos

--- app/rollup/plugins-shared.js
+++ app/rollup/plugins-shared.js
@@ -1,19 +1,17 @@
 import svelte from 'rollup-plugin-svelte'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-//import buble from 'rollup-plugin-buble'
-import babel from 'rollup-plugin-babel'
-import fs from 'fs'
-const babelConfig = JSON.parse(fs.readFileSync(`${__dirname}/../.babelrc`))
+import buble from 'rollup-plugin-buble'
 
 export default () => [
   svelte({ cascade: false }),
-  // TODO: use buble when #81 is fixed https://github.com/Rich-Harris/buble/issues/81
-  //buble({ 'objectAssign': 'Object.assign' }),
-  //babel(Object.assign({ babelrc: false }, babelConfig)),
-  babel(),
+  buble({ 'objectAssign': 'Object.assign' }),
   resolve({
-    module: true, browser: true, jsnext: true, main: true, extensions: [ '.js', '.json' ]
+    module: true,
+    browser: true,
+    jsnext: true,
+    main: true,
+    extensions: [ '.js', '.json', '.mjs', '.es' ]
   }),
   commonjs()
 ]
